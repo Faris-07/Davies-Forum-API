e95b7cd344ae5915372827e66bef1453f6fa71f4
Faris <mohammed.uddin@davies-group.com>
2023-08-04 12:31:25 +0100

    Add project files.

---
+++ Davies-Forum-API/Forum.cs
@@ -0,0 +1,11 @@
+﻿namespace Davies_Forum_API
+{
+    public class Forum
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Office { get; set; } = string.Empty;
+    }
+}
