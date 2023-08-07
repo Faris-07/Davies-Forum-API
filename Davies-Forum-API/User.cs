bdf77cdc0dc3a41b8fc8863285177bb1eff4bdbe
Faris <mohammed.uddin@davies-group.com>
2023-08-07 16:43:12 +0100

    Add User model, register and login authentication and JWT token

---
+++ Davies-Forum-API/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Davies_Forum_API
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
